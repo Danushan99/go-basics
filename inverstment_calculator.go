d9051e94b966b420ce86879841999bd13fd9efd2
Danushan99 <danuthasan08@gmail.com>
2025-03-21 10:40:01 +0530

    feat: implement future value calculation in investment calculator

--- inverstment_calculator.go
+++ inverstment_calculator.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Print("hello world\n")
+	var inverstmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 5
+
+	var futureValue = float64(inverstmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Println(futureValue)
 }
