f32fd544e11f4f93f5dc7f8e87d0616140156e42
Danushan99 <danuthasan08@gmail.com>
2025-03-24 11:42:15 +0530

    fix: correct variable types for investment amount and years in calculation

--- inverstment_calculator.go
+++ inverstment_calculator.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var inverstmentAmount = 1000
+	var inverstmentAmount float64 = 1000
 	var expectedReturnRate = 5.5
-	var years = 5
+	var years float64 = 5
 
-	var futureValue = float64(inverstmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	var futureValue = inverstmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Println(futureValue)
 }
