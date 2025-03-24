557dd50b57bf81f12023bf331b8775872a265e67
Danushan99 <danuthasan08@gmail.com>
2025-03-24 12:22:06 +0530

    fix: update variable declaration for expected return rate in investment calculator

--- inverstment_calculator.go
+++ inverstment_calculator.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var inverstmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
+	expectedReturnRate := 5.5
 	var years float64 = 5
 
 	var futureValue = inverstmentAmount * math.Pow(1+expectedReturnRate/100, years)
