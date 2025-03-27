77dcc6e55a387562a24f91c7e27be5c1f6a3c230
Danushan99 <danuthasan08@gmail.com>
2025-03-27 22:26:45 +0530

    feat: enhance investment calculator to include user input and future real value calculation

--- inverstment_calculator.go
+++ inverstment_calculator.go
@@ -6,10 +6,24 @@ import (
 )
 
 func main() {
-	var inverstmentAmount float64 = 1000
+	const infalationRate = 2.1
+	var inverstmentAmount float64
 	expectedReturnRate := 5.5
-	var years float64 = 5
+	var years float64
+
+	fmt.Print("inverstment Amount : ")
+	fmt.Scan(&inverstmentAmount)
+
+	fmt.Print("Enter Years  : ")
+	fmt.Scan(&years)
+
+	fmt.Print("Enter Return Rate  : ")
+	fmt.Scan(&expectedReturnRate)
 
 	var futureValue = inverstmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	fmt.Println(futureValue)
+	futureRealValue := futureValue / math.Pow(1+infalationRate/100, years)
+	//formated string
+	fmt.Println("Future Value :", futureValue)
+	fmt.Println("Future Real Value :", futureRealValue)
+
 }
