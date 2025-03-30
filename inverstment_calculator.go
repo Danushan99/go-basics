946d1b35a1d8ff29ec9e49d6ac02f9762210ff2c
Danushan99 <danuthasan08@gmail.com>
2025-03-30 19:50:09 +0530

    uncomments

--- inverstment_calculator.go
+++ inverstment_calculator.go
@@ -1,31 +1,31 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"math"
-// )
+import (
+	"fmt"
+	"math"
+)
 
-// func main() {
-// 	const infalationRate = 2.1
-// 	var inverstmentAmount float64
-// 	expectedReturnRate := 5.5
-// 	var years float64
+func main() {
+	const infalationRate = 2.1
+	var inverstmentAmount float64
+	expectedReturnRate := 5.5
+	var years float64
 
-// 	fmt.Print("inverstment Amount : ")
-// 	fmt.Scan(&inverstmentAmount)
+	fmt.Print("inverstment Amount : ")
+	fmt.Scan(&inverstmentAmount)
 
-// 	fmt.Print("Enter Years  : ")
-// 	fmt.Scan(&years)
+	fmt.Print("Enter Years  : ")
+	fmt.Scan(&years)
 
-// 	fmt.Print("Enter Return Rate  : ")
-// 	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Enter Return Rate  : ")
+	fmt.Scan(&expectedReturnRate)
 
-// 	var futureValue = inverstmentAmount * math.Pow(1+expectedReturnRate/100, years)
-// 	futureRealValue := futureValue / math.Pow(1+infalationRate/100, years)
-// 	//formated string - output print
-// 	fmt.Println("Future Value :", futureValue)
+	var futureValue = inverstmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+infalationRate/100, years)
+	//formated string - output print
+	fmt.Println("Future Value :", futureValue)
 
-// 	fmt.Printf("Future Value :%v\n\n ", futureValue)
-// 	fmt.Println("Future Real Value :", futureRealValue)
+	fmt.Printf("Future Value :%v\n\n ", futureValue)
+	fmt.Println("Future Real Value :", futureRealValue)
 
-// }
+}
